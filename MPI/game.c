63d3b0856b0af098b84f0974060df35a3eb946d6
aspil <sdi1700147@di.uoa.gr>
2020-10-09 22:26:58 +0300

    Finished MPI

--- MPI/game.c
+++ MPI/game.c
@@ -6,77 +6,25 @@
 #define ALIVE 1
 #define DEAD 0
 
-
-// void evolve(char** cur, char** next, int pos_i, int pos_j){       // should this return char type? (DEAD/ALIVE)
-// 	// dunno yet in what form the grid 'next' will be
-// 	int alive_neighbors = 0;
-// 	char ALIVE = 'x', DEAD = '-';
-	 
-// 	if(cells[pos_i][pos_j] == ALIVE){
-// 		// 0-1 or 4-8 => die (underpop or overpop)
-// 		// 2-3 => live
-// 		if(cells[pos_i-1][pos_j-1] == ALIVE) alive_neighbors++;
-// 		if(cells[pos_i-1][pos_j] == ALIVE) alive_neighbors++;
-// 		if(cells[pos_i-1][pos_j+1] == ALIVE) alive_neighbors++;
-// 		if(cells[pos_i][pos_j-1] == ALIVE){
-// 			alive_neighbors++;
-// 			if(alive_neighbors == 4){
-// 				next[pos_i][pos_j] = DEAD;  // die due to overpopulation
-// 				return;
-// 			}
-// 		}
-// 		if(cells[pos_i][pos_j+1] == ALIVE){
-// 			alive_neighbors++;
-// 			if(alive_neighbors >= 4){
-// 				next[pos_i][pos_j] = DEAD;  // die due to overpopulation
-// 				return;
-// 			}
-// 		}
-// 		if(cells[pos_i+1][pos_j-1] == ALIVE){
-// 			alive_neighbors++;
-// 			if(alive_neighbors >= 4){
-// 				next[pos_i][pos_j] = DEAD;  // die due to overpopulation
-// 				return;
-// 			}
-// 		}
-// 		if(cells[pos_i+1][pos_j] == ALIVE){
-// 			alive_neighbors++;
-// 			if(alive_neighbors >= 4){
-// 				next[pos_i][pos_j] = DEAD;  // die due to overpopulation
-// 				return;
-// 			}
-// 		}
-// 		if(cells[pos_i+1][pos_j+1] == ALIVE){
-// 			alive_neighbors++;
-// 			if(alive_neighbors >= 4){
-// 				next[pos_i][pos_j] = DEAD;  // die due to overpopulation
-// 				return;
-// 			}
-// 		}
-
-// 		if(alive_neighbors < 2){
-// 			next[pos_i][pos_j] = DEAD; // die due to underpopulation
-// 			return;
-// 		} else {
-// 			next[pos_i][pos_j] = ALIVE; // survive => 2 or 3 alive neighbors
-// 			return;
-// 		}
-// 	}
-// }
-
-
 int main(int argc, char *argv[]){
 	srand(time(0));
 
 	MPI_Comm cartesian;     // will be the new communicator
-	int grid_width = 64, comm_sz;       //maybe grid_width should be passed by arg, e.g 64
+	int grid_width, comm_sz;       //maybe grid_width should be passed by arg, e.g 64
 	int my_rank, neighbour_rank, src, dest;
 	int comm_dims[2];       // dimensions of communicator shape, initialized to 0,0
 	int period[2] = {1,1};       // in this implementation, we work on a simple-grid-shaped communicator
-	int generation = 0;
+	
 	int width_local, height_local;      // width x length of this process's chunk, initialized to 0x0
 	int i, j;       // declaring here, so it only costs once
 	// int chunk_pos[2];       // position of the chunk in the global grid of cells, initialized to 0,0
+
+
+	if (argc != 2) {
+		printf("Wrong program arguments. Usage: mpirun [-n] [X] game.x <grid size>\n");
+		return EXIT_FAILURE;
+	}
+	grid_width = atoi(argv[1]);
 
 	MPI_Init(NULL, NULL);   
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
@@ -85,11 +33,10 @@ int main(int argc, char *argv[]){
 	MPI_Dims_create(comm_sz, 2, comm_dims);    // determines a suitable distribution of processes, according to comm_sz
 	MPI_Cart_create(MPI_COMM_WORLD, 2 , comm_dims, period, 0, &cartesian);      // create a new communicator with topology information
 
-
 	height_local = (int) (grid_width / comm_dims[0]);      // casting is not actually necessary, everything is a power of 2
 	width_local = (int) (grid_width / comm_dims[1]);
-	
 	/* Allocate the 'before' and 'after' grids */
+	// printf("EDW1\n");
 	char **current = malloc((height_local+2) * sizeof(char*));
 	char *current0 = malloc((height_local+2) * (width_local+2) * sizeof(char));
 
@@ -102,13 +49,10 @@ int main(int argc, char *argv[]){
 	}
 
 	/* Initialize the 'before' or current subgrid */
-	for (i = 1; i < height_local + 1; i++) {      // initialize cells; let's see if grid[i][j] works  
-		for (j = 1; j < width_local + 1; j++) {
-			current[i][j] = (rand() % 10) < 7 ? DEAD : ALIVE;       // dead : alive ratio 7:3
-			// printf("%c", current[i][j]);
-		}
-		// printf("\n");
-	}
+	for (i = 1; i < height_local + 1; i++)
+		for (j = 1; j < width_local + 1; j++)
+			current[i][j] = (rand() % 10) < 7 ? DEAD : ALIVE;	// dead : alive ratio 7:3
+
 	/* Get the coordinates for the current process */
 	int my_coords[2];
 	MPI_Cart_coords(cartesian, my_rank, 2, my_coords);
@@ -151,52 +95,57 @@ int main(int argc, char *argv[]){
 	
 	MPI_Request SRequests[8];
 	MPI_Request RRequests[8];
-
 	/* Northern process communication */
-	MPI_Send_init(&current[1][1], width_local, MPI_CHAR, neighbours[0], 0, cartesian, &SRequests[0]);
-	MPI_Recv_init(&current[0][1], width_local, MPI_CHAR, neighbours[0], 0, cartesian, &RRequests[0]);
-	
+	MPI_Recv_init(&(current[0][1]), width_local, MPI_CHAR, neighbours[0], 0, cartesian, &RRequests[0]);
+	MPI_Send_init(&(current[1][1]), width_local, MPI_CHAR, neighbours[0], 0, cartesian, &SRequests[0]);
+
 	/* Southern process communication */
-	MPI_Send_init(&current[height_local][1],   width_local, MPI_CHAR, neighbours[1], 1, cartesian, &SRequests[1]);
-	MPI_Recv_init(&current[height_local+1][1], width_local, MPI_CHAR, neighbours[1], 1, cartesian, &RRequests[1]);
+	MPI_Recv_init(&(current[height_local+1][1]), width_local, MPI_CHAR, neighbours[1], 0, cartesian, &RRequests[1]);
+	MPI_Send_init(&(current[height_local][1]),   width_local, MPI_CHAR, neighbours[1], 0, cartesian, &SRequests[1]);
 
 	/* Western process communication */
-	MPI_Send_init(&current[1][1], 1, column, neighbours[2], 2, cartesian, &SRequests[2]);
-	MPI_Recv_init(&current[1][0], 1, column, neighbours[2], 2, cartesian, &RRequests[2]);
+	MPI_Recv_init(&(current[1][0]), 1, column, neighbours[2], 0, cartesian, &RRequests[2]);
+	MPI_Send_init(&(current[1][1]), 1, column, neighbours[2], 0, cartesian, &SRequests[2]);
 	
 	/* Easter process communication */
-	MPI_Send_init(&current[1][width_local],  	1, column, neighbours[3], 3, cartesian, &SRequests[3]);
-	MPI_Recv_init(&current[1][width_local+1], 	1, column, neighbours[3], 3, cartesian, &RRequests[3]);
+	MPI_Recv_init(&(current[1][width_local+1]), 1, column, neighbours[3], 0, cartesian, &RRequests[3]);
+	MPI_Send_init(&(current[1][width_local]),  	1, column, neighbours[3], 0, cartesian, &SRequests[3]);
 
 	/* North-western process communication */
-	MPI_Send_init(&current[1][1], 1, MPI_CHAR, neighbours[4], 4, cartesian, &SRequests[4]);
-	MPI_Recv_init(&current[0][0], 1, MPI_CHAR, neighbours[4], 4, cartesian, &RRequests[4]);
+	MPI_Recv_init(&(current[0][0]), 1, MPI_CHAR, neighbours[4], 0, cartesian, &RRequests[4]);
+	MPI_Send_init(&(current[1][1]), 1, MPI_CHAR, neighbours[4], 0, cartesian, &SRequests[4]);
 	
 	/* North-eastern process communication */
-	MPI_Send_init(&current[width_local][1],	  1, MPI_CHAR, neighbours[5], 5, cartesian, &SRequests[5]);
-	MPI_Recv_init(&current[0][width_local+1], 1, MPI_CHAR, neighbours[5], 5, cartesian, &RRequests[5]);
+	MPI_Recv_init(&(current[0][width_local+1]), 1, MPI_CHAR, neighbours[5], 0, cartesian, &RRequests[5]);
+	MPI_Send_init(&(current[1][width_local]),   1, MPI_CHAR, neighbours[5], 0, cartesian, &SRequests[5]);
 	
 	/* South-western process communication */
-	MPI_Send_init(&current[height_local][1],   1, MPI_CHAR, neighbours[6], 6, cartesian, &SRequests[6]);
-	MPI_Recv_init(&current[height_local+1][0], 1, MPI_CHAR, neighbours[6], 6, cartesian, &RRequests[6]);
+	MPI_Recv_init(&(current[height_local+1][0]), 1, MPI_CHAR, neighbours[6], 0, cartesian, &RRequests[6]);
+	MPI_Send_init(&(current[height_local][1]),   1, MPI_CHAR, neighbours[6], 0, cartesian, &SRequests[6]);
 	
 	/* South-eastern process communication */
-	MPI_Send_init(&current[height_local][width_local], 	   1, MPI_CHAR, neighbours[7], 7, cartesian, &SRequests[7]);
-	MPI_Recv_init(&current[height_local+1][width_local+1], 1, MPI_CHAR, neighbours[7], 7, cartesian, &RRequests[7]);
+	MPI_Recv_init(&(current[height_local+1][width_local+1]), 1, MPI_CHAR, neighbours[7], 0, cartesian, &RRequests[7]);
+	MPI_Send_init(&(current[height_local][width_local]), 	 1, MPI_CHAR, neighbours[7], 0, cartesian, &SRequests[7]);
 	
-	// MPI_Status status[8];
+	if(my_rank == 0){
+		printf("Before\n");
+		for(i = 0; i < height_local + 2; i++){
+			for(j = 0; j < width_local + 2; j++){
+				printf("%c ", current[i][j] == ALIVE ? 'x' : '_');
+			}
+			printf("\n");
+		}
+		printf("\n\n");
+	}
+	char **temp;
 	int alive_neighbors = 0;
-	while (generation < 1) {
+	int generation = 0;
+	while (generation < 500) {
 		MPI_Startall(8, RRequests);
 		MPI_Startall(8, SRequests);
-
-		/* evolution of inner (white) cells */
-		// alive_neighbors = 0;
+		/* Εvolution of inner (white) cells */
 		for(i = 2; i < height_local; i++){
 			for(j = 2; j < width_local; j++){
-				// 0-1 or 4-8 => die (underpop or overpop)
-				// 2-3 => live
-				// exactly 3 & DEAD => new
 				alive_neighbors = current[i-1][j-1] +
 								  current[i-1][j+1] +
 								  current[i][j-1]	+
@@ -204,8 +153,7 @@ int main(int argc, char *argv[]){
 								  current[i][j+1]   +
 								  current[i+1][j-1] +
 								  current[i+1][j]   +
-								  current[i+1][j+1] -
-								  alive_neighbors;
+								  current[i+1][j+1];
 				if(current[i][j] == ALIVE)
 					next[i][j] = ((alive_neighbors == 2) || (alive_neighbors == 3)) ? ALIVE : DEAD;
 				
@@ -215,10 +163,8 @@ int main(int argc, char *argv[]){
 		}
 
 		MPI_Waitall(8,  RRequests, MPI_STATUSES_IGNORE);
-		// alive_neighbors = 0;
-		/* TODO υπολογισμος εξωτερικων κελιων */
-		for(i = 1; i < height_local + 1; i++){   // calculating the green columns
 
+		for(i = 1; i < height_local + 1; i++){   // calculating the green columns
 			/*-------------  Leftmost column -------------*/
 			alive_neighbors = current[i-1][0] +
 						      current[i-1][1] +
@@ -227,12 +173,10 @@ int main(int argc, char *argv[]){
 							  current[i][2]   +
 							  current[i+1][0] +
 							  current[i+1][1] +
-							  current[i+1][2] -
-							  alive_neighbors;
+							  current[i+1][2];
 
 			if(current[i][1] == ALIVE)
 				next[i][1] = ((alive_neighbors == 2) || (alive_neighbors == 3)) ? ALIVE : DEAD;
-			
 			else
 				next[i][1] = (alive_neighbors == 3) ? ALIVE : DEAD;
 			
@@ -244,8 +188,7 @@ int main(int argc, char *argv[]){
 							  current[i][width_local+1]   +
 							  current[i+1][width_local-1] +
 							  current[i+1][width_local]   +
-							  current[i+1][width_local+1] -
-							  alive_neighbors;
+							  current[i+1][width_local+1];
 
 			if(current[i][width_local] == ALIVE)
 				next[i][width_local] = ((alive_neighbors == 2) || (alive_neighbors == 3)) ? ALIVE : DEAD;
@@ -254,10 +197,8 @@ int main(int argc, char *argv[]){
 			
 		}
 
-		// alive_neighbors = 0;
 		for(j = 1; j < width_local + 1; j++){   // calculating the green rows
-			
-		//////////////  UPPERMOST ROW //////////////
+			/* -------------- Uppermost row --------------*/
 			alive_neighbors = current[0][j-1] +
 						      current[0][j]   +
 							  current[0][j+1] +
@@ -265,46 +206,38 @@ int main(int argc, char *argv[]){
 							  current[1][j+1] +
 							  current[2][j-1] +
 							  current[2][j]   +
-							  current[2][j+1] -
-							  alive_neighbors;
+							  current[2][j+1];
 
 			if(current[1][j] == ALIVE)
 				next[1][j] = ((alive_neighbors == 2) || (alive_neighbors == 3)) ? ALIVE : DEAD;
 			else
 				next[1][j] = (alive_neighbors == 3) ? ALIVE : DEAD;
 			
-			//////////////  LOWERMOST ROW //////////////
-			alive_neighbors = current[width_local-1][j-1] +
-						      current[width_local-1][j]   +
-							  current[width_local-1][j+1] +
-							  current[width_local][j-1] +
-							  current[width_local][j+1] +
-							  current[width_local+1][j-1] +
-							  current[width_local+1][j]   +
-							  current[width_local+1][j+1] -
-							  alive_neighbors;
+			/* -------------- Lowermost row --------------*/
+			alive_neighbors = current[height_local-1][j-1] +
+						      current[height_local-1][j]   +
+							  current[height_local-1][j+1] +
+							  current[height_local][j-1]   +
+							  current[height_local][j+1]   +
+							  current[height_local+1][j-1] +
+							  current[height_local+1][j]   +
+							  current[height_local+1][j+1];
 
-			if(current[width_local][j] == ALIVE)
-				next[width_local][j] = ((alive_neighbors == 2) || (alive_neighbors == 3)) ? ALIVE : DEAD;
+			if(current[height_local][j] == ALIVE)
+				next[height_local][j] = ((alive_neighbors == 2) || (alive_neighbors == 3)) ? ALIVE : DEAD;
 			else
-				next[width_local][j] = (alive_neighbors == 3) ? ALIVE : DEAD;
+				next[height_local][j] = (alive_neighbors == 3) ? ALIVE : DEAD;
 		}
 
 		MPI_Waitall(8,  SRequests, MPI_STATUSES_IGNORE);
-		generation++;        
-	}
+		generation++; 
+		temp = current;
+		current = next;
+		next = temp;       
+	}	/* End of main loop */
 
 	if(my_rank == 0){
-
-		//////////////// PRINT CURRENT ////////////////
-		for(i = 0; i < height_local + 2; i++){
-			for(j = 0; j < width_local + 2; j++){
-				printf("%c ", current[i][j] == ALIVE ? 'x' : '_');
-			}
-			printf("\n");
-		}
-		printf("\n\n");
-		//////////////// PRINT NEXT ////////////////
+		printf("After\n");
 		for(i = 0; i < height_local + 2; i++){
 			for(j = 0; j < width_local + 2; j++){
 				printf("%c ", next[i][j] == ALIVE ? 'x' : '_');
@@ -312,13 +245,13 @@ int main(int argc, char *argv[]){
 			printf("\n");
 		}
 	}
-
+	MPI_Type_free(&column);
 	MPI_Comm_free(&cartesian);
 	MPI_Finalize();
 	
-	free(current[0]);
-	free(current);
-	free(next[0]);
-	free(next);
+	// free(current[0]);
+	// free(current);
+	// free(next[0]);
+	// free(next);
 	return 0;
 }

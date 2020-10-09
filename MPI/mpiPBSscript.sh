90f47d1b3dee0e8758c5e22c332d8e5c230d1786
aspil <sdi1700147@di.uoa.gr>
2020-10-10 01:19:41 +0300

    Fixes

--- MPI/mpiPBSscript.sh
+++ MPI/mpiPBSscript.sh
@@ -7,7 +7,7 @@
 #PBS -l walltime=00:00:40
 
 # How many nodes and tasks per node, Example 2 nodes with 8 tasks each => 16 tasts #
-#PBS -l select=8:ncpus=8:mpiprocs=8
+#PBS -l select=1:ncpus=8:mpiprocs=8
 
 # Only this job uses the chosen nodes
 #PBS -l place=excl
@@ -16,9 +16,9 @@
 #PBS -q workq
 
 # JobName #
-#PBS -N myJob_64_640
+#PBS -N myJob_8_320
 
 #Change Working directory to SUBMIT directory
 cd $PBS_O_WORKDIR
 # Run executable #
-mpirun game_reduce.x 640
+mpirun game_mpi.x 320
--- MPI/sub.sh
+++ MPI/sub.sh
@@ -2,7 +2,7 @@
 
 n_proc=$1
 grid_sz=$2
-directive=$3
+executable=$3
 if [ $n_proc -le 0 ]; then
 	echo "[$(whoami)]: Please provide a positive number of processes."
 	exit
@@ -13,24 +13,24 @@ res_ceil=$(echo "$res" | perl -nl -MPOSIX -e 'print ceil($_);')
 res_floor=$(echo "$res" | perl -nl -MPOSIX -e 'print floor($_);')
 
 if [[ $res_ceil != $res_floor ]]; then
-	echo "[$(whoami)]: $1 is not a power of 2. Please give a number that is a power of 2."
+	echo "[$(whoami)]: $n_proc is not a power of 2. Please give a number that is a power of 2."
 	exit
 fi
 
 # Remove the switch argument from mpirun command on mpiPBSscript.sh
-sed -i -e 's/mpirun -n [0-9]* game[a-zA-Z_]*.x [0-9]*/mpirun game_'$directive'.x '$2'/g' mpiPBSscript.sh
+sed -i -e 's/mpirun -n [0-9]* .*.x [0-9]*/mpirun '$executable'.x '$grid_sz'/g' mpiPBSscript.sh
 
 if [[ n_proc -le 4 ]]; then
 	sed -i -e 's/select=[0-9]*/select=1/g' mpiPBSscript.sh
 	sed -i -e 's/mpiprocs=[0-9]*/mpiprocs='$n_proc'/g' mpiPBSscript.sh
-	sed -i -e 's/mpirun game[a-zA-Z_]*.x [0-9]*/mpirun -n '$n_proc' game_'$directive'.x '$grid_sz'/g' mpiPBSscript.sh
+	sed -i -e 's/mpirun .*.x [0-9]*/mpirun -n '$n_proc' '$executable'.x '$grid_sz'/g' mpiPBSscript.sh
 else
 	nodes=$(($n_proc / 8))
 	sed -i -e 's/select=[0-9]*/select='$nodes'/g' mpiPBSscript.sh
 	sed -i -e 's/mpiprocs=[0-9]*/mpiprocs=8/g' mpiPBSscript.sh
 fi
 sed -i -e 's/#PBS -N myJob_[0-9]*_[0-9]*/#PBS -N myJob_'$n_proc'_'$grid_sz'/g' mpiPBSscript.sh
-cat mpiPBSscript.sh | grep mpirun
+# cat mpiPBSscript.sh | grep mpirun
 echo
 # Finally sumbit the job on the PBS queue
-# qsub mpiPBSscript.sh
+qsub mpiPBSscript.sh

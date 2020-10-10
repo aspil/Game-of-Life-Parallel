82afa517b6a874e6adede3db7a6d7d48405ee8e7
aspil <sdi1700147@di.uoa.gr>
2020-10-10 19:36:58 +0300

    .

--- MPI/mpiPBSscript.sh
+++ MPI/mpiPBSscript.sh
@@ -6,19 +6,23 @@
 # Max Wall time, Example 1 Minute #
 #PBS -l walltime=00:00:40
 
-# How many nodes and tasks per node, Example 2 nodes with 8 tasks each => 16 tasts #
-#PBS -l select=1:ncpus=8:mpiprocs=8
+#=====================================================
 
+# How many nodes and tasks per node, Example 2 nodes with 8 tasks each => 16 tasts #
+#PBS -l select=1:ncpus=8:mpiprocs=1:ompthreads=8
+
+# JobName #
+#PBS -N hybrid_1_8_320
+
+#====================================================
 # Only this job uses the chosen nodes
 #PBS -l place=excl
 
 # Which Queue to use, DO NOT CHANGE #
 #PBS -q workq
 
-# JobName #
-#PBS -N myJob_8_320
 
 #Change Working directory to SUBMIT directory
 cd $PBS_O_WORKDIR
 # Run executable #
-mpirun game_mpi.x 320
+mpirun -n 1 game_hybrid.x -s 320 -t 8
